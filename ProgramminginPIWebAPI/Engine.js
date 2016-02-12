a8c5e56be84bc92428944e188577b8f9a9d12d5d
james9005 <james9005@hotmail.co.uk>
2016-02-12 17:06:56 +0000

    added seperate javascript file

---
+++ ProgramminginPIWebAPI/Engine.js
@@ -0,0 +1,12 @@
+﻿function getJsonData(webAPIURL) {
+
+    $.ajax({
+        headers: { "Content-Type": "application/json; charset=utf-8", },
+        url: 'https://JDTSQL01/piwebapi',
+        dataType: 'json',
+        type: 'GET',
+    }).done(function (response) {
+                
+       return response
+    });
+});
