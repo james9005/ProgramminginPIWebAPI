b2e9acaf03894d5013be38bd1ace6b937505601f
james9005 <james9005@hotmail.co.uk>
2016-02-12 21:52:01 +0000

    added the makeAjaxRequest method

--- ProgramminginPIWebAPI/Engine.js
+++ ProgramminginPIWebAPI/Engine.js
@@ -3,7 +3,7 @@
 //TODO: methods need to be more general and in reference to the PI AF Structure for server overhauls
 //Because AJAX is asyncronous, the value returned will always be undefined (uses different browser thread). Because of this the logic will have to be applied within the method rather than in the HTML page.
 function getAFServers(webAPIURL) {
-    
+
     $.ajax({
         headers: { "Content-Type": "application/json; charset=utf-8", },
         url: webAPIURL,
@@ -14,13 +14,37 @@ function getAFServers(webAPIURL) {
 
         console.log(response);
 
-        for(var i = 0; i < response.Items.length; i++){
-            document.getElementById("select").innerHTML += "<option id =" + i + ">"+ response.Items[i].Name + "</option>";
+        for (var i = 0; i < response.Items.length; i++) {
+            document.getElementById("select").innerHTML += "<option id =" + i + ">" + response.Items[i].Name + "</option>";
         }
-        
-        
+
+
+        MakeAjaxRequest('GET', webAPIURL, function (data) {
+
+            console.log(data);
+        });
+
 
         //TODO:main logic goes here.
-        
+
+    });
+}
+/**
+The Make Ajax Request returns a data object (JSON format) back to the caller
+*/
+function MakeAjaxRequest(type, url, SuccessCallBack, data) {
+    $.ajax({
+        type: type,
+        url: url,
+
+        cache: false,
+        async: true,
+        data: data,
+        contentType: "application/json",
+        success: SuccessCallBack,
+        error: (function (error, variable) {
+            console.log(error);
+            alert('There was an error with the request');
+        })
     });
 }
