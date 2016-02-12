8fc1579fed34727739be8a004e3cebd403713c56
james9005 <james9005@hotmail.co.uk>
2016-02-12 20:52:00 +0000

    more work to set the environment up

--- ProgramminginPIWebAPI/Engine.js
+++ ProgramminginPIWebAPI/Engine.js
@@ -1,8 +1,8 @@
-﻿// the engine class sorts out the calls to the PI WEB API.
+﻿//The engine class sorts out the calls to the PI WEB API.
 
 //TODO: methods need to be more general and in reference to the PI AF Structure for server overhauls
 //Because AJAX is asyncronous, the value returned will always be undefined (uses different browser thread). Because of this the logic will have to be applied within the method rather than in the HTML page.
-function getJsonData(webAPIURL) {
+function getAFServers(webAPIURL) {
     
     $.ajax({
         headers: { "Content-Type": "application/json; charset=utf-8", },
@@ -13,6 +13,12 @@ function getJsonData(webAPIURL) {
         console.log('RESPONSE FROM JSON METHOD :');
 
         console.log(response);
+
+        for(var i = 0; i < response.Items.length; i++){
+            document.getElementById("select").innerHTML += "<option id =" + i + ">"+ response.Items[i].Name + "</option>";
+        }
+        
+        
 
         //TODO:main logic goes here.
         
