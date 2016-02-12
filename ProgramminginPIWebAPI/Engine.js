c8e94b005c63c0b2758a5e718b63a79dee68a84f
james9005 <james9005@hotmail.co.uk>
2016-02-12 18:55:44 +0000

    sorted some of the logic out with ajax

--- ProgramminginPIWebAPI/Engine.js
+++ ProgramminginPIWebAPI/Engine.js
@@ -1,14 +1,20 @@
-﻿//TODO: methods need to be more general and in reference to the PI AF Structure for server overhauls
+﻿// the engine class sorts out the calls to the PI WEB API.
 
+//TODO: methods need to be more general and in reference to the PI AF Structure for server overhauls
+//Because AJAX is asyncronous, the value returned will always be undefined (uses different browser thread). Because of this the logic will have to be applied within the method rather than in the HTML page.
 function getJsonData(webAPIURL) {
-
+    
     $.ajax({
         headers: { "Content-Type": "application/json; charset=utf-8", },
         url: webAPIURL,
         dataType: 'json',
         type: 'GET',
     }).done(function (response) {
-        console.log('RESPONSE FROM JSON METHOD :' + response);
-        return response
+        console.log('RESPONSE FROM JSON METHOD :');
+
+        console.log(response);
+
+        //TODO:main logic goes here.
+        
     });
 }
