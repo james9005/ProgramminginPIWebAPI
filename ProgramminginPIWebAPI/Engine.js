95b6f7bc9975f843317b68f115ede7f4c988b04a
james9005 <james9005@hotmail.co.uk>
2016-02-17 22:07:07 +0000

    CPU Temp graph is now a global method

--- ProgramminginPIWebAPI/Engine.js
+++ ProgramminginPIWebAPI/Engine.js
@@ -50,92 +50,92 @@ function MakeAjaxRequest(type, url, SuccessCallBack, data) {
 /** 
 The updateCPUChart function update the large CPU area within the main area of the page.
 */
-$(
-    function updateCPUChart() {
 
-        // Get context with jQuery - using jQuery's .get() method.
-        var salesChartCanvas = $("#salesChart").get(0).getContext("2d");
-        // This will get the first returned node in the jQuery collection.
-        var salesChart = new Chart(salesChartCanvas);
+function updateCPUChart() {
 
-        var salesChartData = {
-            labels: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00"],
-            datasets: [
-              {
-                  label: "Electronics",
-                  fillColor: "rgb(210, 214, 222)",
-                  strokeColor: "rgb(210, 214, 222)",
-                  pointColor: "rgb(210, 214, 222)",
-                  pointStrokeColor: "#c1c7d1",
-                  pointHighlightFill: "#fff",
-                  pointHighlightStroke: "rgb(220,220,220)",
-                  //fill with random data.
-                  data: [(Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100)]
-              },
-              {
-                  label: "Digital Goods",
-                  fillColor: "rgba(60,141,188,0.9)",
-                  strokeColor: "rgba(60,141,188,0.8)",
-                  pointColor: "#3b8bba",
-                  pointStrokeColor: "rgba(60,141,188,1)",
-                  pointHighlightFill: "#fff",
-                  pointHighlightStroke: "rgba(60,141,188,1)",
-                  //fill with random data
-                  data: [(Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100)]
-              }
-            ]
-        };
+    // Get context with jQuery - using jQuery's .get() method.
+    var salesChartCanvas = $("#salesChart").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var salesChart = new Chart(salesChartCanvas);
 
-        var salesChartOptions = {
-            //Boolean - If we should show the scale at all
-            showScale: true,
-            //Boolean - Whether grid lines are shown across the chart
-            scaleShowGridLines: false,
-            //String - Colour of the grid lines
-            scaleGridLineColor: "rgba(0,0,0,.05)",
-            //Number - Width of the grid lines
-            scaleGridLineWidth: 1,
-            //Boolean - Whether to show horizontal lines (except X axis)
-            scaleShowHorizontalLines: true,
-            //Boolean - Whether to show vertical lines (except Y axis)
-            scaleShowVerticalLines: true,
-            //Boolean - Whether the line is curved between points
-            bezierCurve: true,
-            //Number - Tension of the bezier curve between points
-            bezierCurveTension: 0.3,
-            //Boolean - Whether to show a dot for each point
-            pointDot: true,
-            //Number - Radius of each point dot in pixels
-            pointDotRadius: 4,
-            //Number - Pixel width of point dot stroke
-            pointDotStrokeWidth: 1,
-            //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-            pointHitDetectionRadius: 20,
-            //Boolean - Whether to show a stroke for datasets
-            datasetStroke: true,
-            //Number - Pixel width of dataset stroke
-            datasetStrokeWidth: 2,
-            //Boolean - Whether to fill the dataset with a color
-            datasetFill: true,
-            //String - A legend template
-            legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%=datasets[i].label%></li><%}%></ul>",
-            //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-            maintainAspectRatio: true,
-            //Boolean - whether to make the chart responsive to window resizing
-            responsive: true,
-            animation: false
-        };
+    var salesChartData = {
+        labels: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00"],
+        datasets: [
+          {
+              label: "Electronics",
+              fillColor: "rgb(210, 214, 222)",
+              strokeColor: "rgb(210, 214, 222)",
+              pointColor: "rgb(210, 214, 222)",
+              pointStrokeColor: "#c1c7d1",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgb(220,220,220)",
+              //fill with random data.
+              data: [(Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100)]
+          },
+          {
+              label: "Digital Goods",
+              fillColor: "rgba(60,141,188,0.9)",
+              strokeColor: "rgba(60,141,188,0.8)",
+              pointColor: "#3b8bba",
+              pointStrokeColor: "rgba(60,141,188,1)",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(60,141,188,1)",
+              //fill with random data
+              data: [(Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100), (Math.random() * 100)]
+          }
+        ]
+    };
 
-        //Create the line chart
-        salesChart.Line(salesChartData, salesChartOptions);
-        //update the chart with random data ever 3000 ms (3 seconds).
-        //TODO: change this to a WEB API call rather than that of random data.
-        //TODO: work out the timing of the pi web API so this doesnt look stupid. (looking at about 3 minutes maybe?).
-        setTimeout(updateCPUChart, 3000);
+    var salesChartOptions = {
+        //Boolean - If we should show the scale at all
+        showScale: true,
+        //Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines: false,
+        //String - Colour of the grid lines
+        scaleGridLineColor: "rgba(0,0,0,.05)",
+        //Number - Width of the grid lines
+        scaleGridLineWidth: 1,
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+        //Boolean - Whether the line is curved between points
+        bezierCurve: true,
+        //Number - Tension of the bezier curve between points
+        bezierCurveTension: 0.3,
+        //Boolean - Whether to show a dot for each point
+        pointDot: true,
+        //Number - Radius of each point dot in pixels
+        pointDotRadius: 4,
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth: 1,
+        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+        pointHitDetectionRadius: 20,
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke: true,
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth: 2,
+        //Boolean - Whether to fill the dataset with a color
+        datasetFill: true,
+        //String - A legend template
+        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%=datasets[i].label%></li><%}%></ul>",
+        //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+        maintainAspectRatio: true,
+        //Boolean - whether to make the chart responsive to window resizing
+        responsive: true,
+        animation: false
+    };
 
-    }
+    //Create the line chart
+    salesChart.Line(salesChartData, salesChartOptions);
+    //update the chart with random data ever 3000 ms (3 seconds).
+    //TODO: change this to a WEB API call rather than that of random data.
+    //TODO: work out the timing of the pi web API so this doesnt look stupid. (looking at about 3 minutes maybe?).
+    setTimeout(updateCPUChart, 3000);
 
-);
+}
+
+
 
 
 //TODO: Work on this
@@ -158,7 +158,7 @@ function updateAllHardware() {
 
     document.getElementById("ramUsage").innerHTML = (currentRamValue + " / " + maxRamValue + " " + ramUnits);
     document.getElementById("ramUsageValue").style.width = ((currentRamValue / maxRamValue) * 100) + '%';
-    document.getElementById("ramUsageValue").setAttribute("class", "progress " + checkHardwareStatus(currentRamValue,maxRamValue));
+    document.getElementById("ramUsageValue").setAttribute("class", "progress " + checkHardwareStatus(currentRamValue, maxRamValue));
 
 
     //C:/
@@ -188,7 +188,7 @@ function updateAllHardware() {
     //Other
     //these values will be replaced with values from the PIWebAPI
     //TODO:this should be set to the correct thing remember not math.random
-    var currentOtherValue = Math.round((Math.random()*500));
+    var currentOtherValue = Math.round((Math.random() * 500));
     var maxOtherValue = 500;
     var otherUnits = "GB";
 
@@ -225,4 +225,5 @@ function checkHardwareStatus(current, max) {
 
 
 //call all methods
+updateCPUChart();
 updateAllHardware();
